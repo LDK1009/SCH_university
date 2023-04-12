ed022b747df65f9761d985a879aea1e9e3e10c4c
Soon_Coding <m3088787@naver.com>
2023-04-12 09:50:30 +0900

    update

---
+++ 컴퓨터소프트웨어공학과/2023-1/컴퓨터그래픽스/컴퓨터그래픽스 실습/05_2 투영 변경/05_2 투영 변경/05_2 투영 변경.cpp
@@ -0,0 +1,67 @@
+﻿#include <glut.h>
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glutWireCube(1.0);
+
+	glPushMatrix();
+	glColor3f(0.0, 1.0, 0.0);
+	glTranslatef(-1.5, 0.0, 1.5);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(0.0, 0.0, 1.0);
+	glTranslatef(1.5, 0.0, 1.5);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glutSwapBuffers();
+
+}
+
+
+
+void init()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void reshape(int new_w, int new_h)
+{
+	glViewport(0, 0, new_w, new_h);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glFrustum(-1.5, 1.5, -1.5, 1.5, 1.5, 20.0);
+	glOrtho(-1.5, 1.5, -1.5, 1.5, 1.5, 20.0);
+}
+
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(100, 100);
+
+	glutCreateWindow("05_2 Push Pop Matrix + projection");
+	init();
+
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+
+	return 0;
+}
