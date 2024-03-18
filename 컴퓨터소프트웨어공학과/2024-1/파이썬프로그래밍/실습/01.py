8f8a87f55822c9ad1a1415cf4621bf2e3015091a
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-03-18 11:09:13 +0900

    0318

---
+++ 컴퓨터소프트웨어공학과/2024-1/파이썬프로그래밍/실습/01.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+x=123
+print("%d" %x)
+print("%5d" %123)
+print("%05d" %123)
+print("%.6f" %123.45)
+print("%7.1f" %123.5)
+print("%7.3f" %123.45)
+print("%s" % "Python")
+print("%10s" % "Python")
